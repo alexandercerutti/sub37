567f1a3eaf0abe7f7a7a6f908a4581d87066c1d6
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-01-11 22:24:57 +0100

    Added first integration of evaluateBlock and renamed parseWebVTTHeaders to evaluateHeaders

--- packages/webvtt-renderer/src/index.ts
+++ packages/webvtt-renderer/src/index.ts
@@ -2,7 +2,11 @@ import { HSBaseRenderer } from "@hsubs/base-renderer";
 import { CueNode } from "@hsubs/server";
 
 const LF_REGEX = /\n/;
-const WEBVTT_HEADER_SECTION = /^\n*WEBVTT(?:\n(.+))?/;
+const WEBVTT_HEADER_SECTION = /^(?:[\uFEFF\n]*)?WEBVTT(?:\n(.+))?/;
+const BLOCK_MATCH_REGEX = /(?<blocktype>(?:REGION|STYLE|NOTE)\s*)/;
+const REGION_ATTRIBUTES_REGEX = /(?:(?<key>[^\s]+):(?<value>[^\s]+))(?:(?:[\r\n]+)|\s+)/g;
+const CUE_MATCH_REGEX =
+	/(?:(?<cueid>\d{1,})[\r\n]+)?(?<starttime>(?:\d\d:?){3}\.\d{3})\s-->\s(?<endtime>(?:\d\d:?){3}(?:\.\d{3}))(?<attributes>.+)?[\r\n]+(?<text>(?:.+[\r\n]*)+)/;
 
 export class WebVTTRenderer extends HSBaseRenderer {
 	static override get supportedType() {
@@ -10,6 +14,7 @@ export class WebVTTRenderer extends HSBaseRenderer {
 	}
 
 	override parse(rawContent: string): CueNode[] {
+		const cues: CueNode[] = [];
 		const content = rawContent.replace(/\r?\n/g, "\n");
 		const block = {
 			start: 0,
@@ -24,7 +29,7 @@ export class WebVTTRenderer extends HSBaseRenderer {
 			block.cursor += 1;
 		} while (!LF_REGEX.test(content[block.cursor]) && !LF_REGEX.test(content[block.cursor + 1]));
 
-		parseWebVTTHeaders(content.substring(block.start, block.cursor + 1));
+		evalutateHeaders(content.substring(block.start, block.cursor + 1));
 
 		block.cursor += 3;
 		block.start = block.cursor - 1;
@@ -42,6 +47,8 @@ export class WebVTTRenderer extends HSBaseRenderer {
 			if (LF_REGEX.test(content[block.cursor]) && LF_REGEX.test(content[block.cursor + 1])) {
 				console.log("Found block:", content.substring(block.start, block.cursor + 1));
 
+				evalutateBlock(content.substring(block.start, block.cursor + 1));
+
 				/** Skipping \n\n and going to the next character */
 				block.cursor += 3;
 				block.start = block.cursor - 1;
@@ -50,14 +57,38 @@ export class WebVTTRenderer extends HSBaseRenderer {
 			}
 		} while (block.cursor <= content.length);
 
-		/** @TODO evaluate the last block cursor + 1 */
+		/** Evaluating the last block found */
+		evalutateBlock(content.substring(block.start, block.cursor + 1));
 
-		return [];
+		return cues;
 	}
 }
 
-function parseWebVTTHeaders(content: string) {
+function evalutateHeaders(content: string) {
 	if (!WEBVTT_HEADER_SECTION.test(content)) {
 		throw new Error("Invalid WebVTT file. It should start with string 'WEBVTT'");
+	}
+}
+
+function evalutateBlock(content: string) {
+	const blockMatch = content.match(BLOCK_MATCH_REGEX);
+
+	if (blockMatch && blockMatch.groups["blockType"]) {
+		switch (blockMatch.groups["blockType"]) {
+			case "REGION":
+				/** @TODO not supported yet */
+				return;
+			case "STYLE":
+				/** @TODO not supported yet */
+				return;
+			case "NOTE":
+				return;
+		}
+	}
+
+	const cueMatch = content.match(CUE_MATCH_REGEX);
+
+	if (cueMatch !== null) {
+		return createVTTCueFromCueMatch(cueMatch, availableRegions);
 	}
 }
