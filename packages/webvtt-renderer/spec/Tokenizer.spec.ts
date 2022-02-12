6335959b658b4d86821ec23f3a2a8d62cbecf624
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-02-13 00:42:25 +0100

    Added initial support to jasmine for tests

---
+++ packages/webvtt-renderer/spec/Tokenizer.spec.ts
@@ -0,0 +1,15 @@
+import { Tokenizer } from "../src/Tokenizer";
+
+describe("Tokenizer", () => {
+	let tokenizer: Tokenizer;
+
+	describe("[static]", () => {
+		describe("parseHTMLEntity", () => {
+			it("should work", () => {
+				expect(true).toBeTruthy();
+			});
+		});
+	});
+
+	xdescribe("[instance]", () => {});
+});
