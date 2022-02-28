bff77df4c751e4fedf1c86ed3975a0c972602372
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-02-28 22:37:09 +0100

    Added more tests for Renderer.parse

--- packages/webvtt-renderer/specs/Renderer.spec.mjs
+++ packages/webvtt-renderer/specs/Renderer.spec.mjs
@@ -14,4 +14,114 @@ describe("WebVTTRenderer", () => {
 	it("should always return a supported type", () => {
 		chai.expect(WebVTTRenderer.supportedType).to.eql("text/vtt");
 	});
+
+	describe("parse", () => {
+		const PureWebVttContent = `
+WEBVTT
+
+00:00:05.000 --> 00:00:25.000 region:fred align:left
+<v Fred&gt;>Would you like to get &lt; coffee?
+
+00:00:00.000 --> 00:00:20.000 region:fred align:left
+<lang.mimmo en-US>Hi, my name is Fred</lang>`;
+
+		const WebVttContentWithTimestamps = `
+WEBVTT
+
+00:00:16.000 --> 00:00:24.000
+<00:00:16.000> <c.mimmo>This</c>
+<00:00:18.000> <c>can</c>
+<00:00:20.000> <c>match</c>
+<00:00:22.000> <c>:past/:future</c>
+<00:00:24.000>
+`;
+
+		it("should return an empty array if rawContent is falsy", () => {
+			chai.expect(renderer.parse(undefined)).to.eql([]);
+			chai.expect(renderer.parse(null)).to.eql([]);
+			chai.expect(renderer.parse("")).to.eql([]);
+		});
+
+		it("should throw if it receives a string that does not start with 'WEBTT' header", () => {
+			const invalidWebVTTError = "Invalid WebVTT file. It should start with string 'WEBVTT'";
+			// @ts-expect-error
+			chai.expect(() => renderer.parse(true)).to.throw(Error, invalidWebVTTError);
+			// @ts-expect-error
+			chai.expect(() => renderer.parse(10)).to.throw(Error, invalidWebVTTError);
+			chai.expect(() => renderer.parse("Look, a phoenix!")).to.throw(Error, invalidWebVTTError);
+		});
+
+		/** @TODO review test. Incongruence in entities type found, between the two returned cues */
+		xit("should return an array containing two cues", () => {
+			const parsingResult = renderer.parse(PureWebVttContent);
+			chai.expect(parsingResult).to.be.an("array");
+			chai.expect(parsingResult.length).to.eql(2);
+
+			console.log(parsingResult);
+
+			chai.expect(parsingResult[0]).to.eql({
+				startTime: 5000,
+				endTime: 25000,
+				content: "Would you like to get < coffee?",
+				id: undefined,
+				entities: [
+					{
+						offset: 0,
+						length: 31,
+						type: 1,
+					},
+				],
+			});
+
+			chai.expect(parsingResult[1]).to.eql({
+				startTime: 0,
+				endTime: 20000,
+				content: "Hi, my name is Fred",
+				id: undefined,
+				entities: [
+					{
+						offset: 0,
+						length: 19,
+						attributes: ["en-US"],
+					},
+				],
+			});
+		});
+
+		it("should return an array containing four cues when a timestamps are found", () => {
+			const parsingResult = renderer.parse(WebVttContentWithTimestamps);
+			chai.expect(parsingResult).to.be.an("array");
+			chai.expect(parsingResult.length).to.eql(5);
+
+			console.log(parsingResult);
+
+			// chai.expect(parsingResult[0]).to.eql({
+			// 	startTime: 5000,
+			// 	endTime: 25000,
+			// 	content: "Would you like to get < coffee?",
+			// 	id: undefined,
+			// 	entities: [
+			// 		{
+			// 			offset: 0,
+			// 			length: 31,
+			// 			type: 1,
+			// 		},
+			// 	],
+			// });
+
+			// chai.expect(parsingResult[1]).to.eql({
+			// 	startTime: 0,
+			// 	endTime: 20000,
+			// 	content: "Hi, my name is Fred",
+			// 	id: undefined,
+			// 	entities: [
+			// 		{
+			// 			offset: 0,
+			// 			length: 19,
+			// 			attributes: ["en-US"],
+			// 		},
+			// 	],
+			// });
+		});
+	});
 });
