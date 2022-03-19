d68b80d88a616ef9fdb8685c5f05d2d73bcbfb38
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-03-19 23:01:35 +0100

    Added support to activeTrack in HSSession

--- packages/server/src/Session.ts
+++ packages/server/src/Session.ts
@@ -2,8 +2,11 @@ import { HSBaseRendererConstructor } from "./BaseRenderer";
 import type { RawTrack } from "./model";
 import { TimelineTree } from "./TimelineTree.js";
 
+const activeTrackSymbol = Symbol("session.active");
+
 export class HSSession {
 	private timelines: { [lang: string]: TimelineTree };
+	private [activeTrackSymbol]: string = null;
 
 	constructor(rawContents: RawTrack[], public renderer: InstanceType<HSBaseRendererConstructor>) {
 		for (let { lang, content } of rawContents) {
@@ -16,5 +19,22 @@ export class HSSession {
 				console.error(err);
 			}
 		}
+
+		if (Object.keys(this.timelines).length) {
+			this.activeTrack = Object.keys(this.timelines)[0];
+		}
+	}
+
+	public get activeTrack() {
+		return this[activeTrackSymbol];
+	}
+
+	public set activeTrack(lang: string) {
+		if (!this.timelines[lang]) {
+			console.warn("Missing language. Active track wasn't set.");
+			return;
+		}
+
+		this[activeTrackSymbol] = lang;
 	}
 }
