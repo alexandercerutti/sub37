35dda1318d3fb1c40e40b69449424d0c3a5231cc
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-04-16 16:09:01 +0200

    Exported Region from server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,3 @@
 export { HSServer } from "./Server.js";
 export { HSBaseRenderer, HSBaseRendererConstructor } from "./BaseRenderer/index.js";
-export type { Entity, CueNode } from "./model";
+export type { Entity, CueNode, Region } from "./model";
