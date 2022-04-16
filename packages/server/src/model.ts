c980426df09ed5291ba41adad3a7715cfd3a692f
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-04-16 16:05:50 +0200

    Changed server's Region model

--- packages/server/src/model.ts
+++ packages/server/src/model.ts
@@ -16,9 +16,28 @@ export interface Region {
 	id: string;
 	width?: string;
 	lines?: number;
-	scroll?: "up" | "none";
-	regionanchor?: [`${number}%`, `${number}%`];
-	viewportanchor?: [`${number}%`, `${number}%`];
+
+	/**
+	 * Describes how new cues should appear if others are
+	 * still visible or should disappear soon.
+	 *
+	 * - Push bottom to up, for roll-up captions
+	 * - Replace current cue. Default, for pop-on captions
+	 *
+	 * Paint-on is achieved by printing one word per time
+	 *
+	 * Default: "replace"
+	 */
+	displayStrategy?: "push" | "replace";
+
+	/**
+	 * (X, Y) of the region relative to root container
+	 * Equivalent to VTT `viewportAnchor` and TTML `tts:origin`
+	 */
+
+	origin?: [`${number}%`, `${number}%`];
+
+	// regionanchor?: [`${number}%`, `${number}%`]; // Is there something like this in other systems outside VTT?
 }
 
 export interface CueNode {
