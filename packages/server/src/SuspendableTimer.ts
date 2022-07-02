9e1c4132429861a3c042beafbb19b16d96b370c0
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-07-02 14:04:12 +0200

    Added ticker implementation on SuspendableTimer to allow executing the callback manually

--- packages/server/src/SuspendableTimer.ts
+++ packages/server/src/SuspendableTimer.ts
@@ -1,14 +1,29 @@
+interface Ticker {
+	run(): void;
+}
+
+function createTicker(tickCallback: () => void) {
+	return {
+		run() {
+			tickCallback();
+		},
+	};
+}
+
 export class SuspendableTimer {
 	private interval: number = null;
+	private ticker: Ticker;
 
-	constructor(private frequency: number, private taskCallback: () => void) {}
+	constructor(private frequency: number, tickCallback: () => void) {
+		this.ticker = createTicker(tickCallback);
+	}
 
 	public start() {
 		if (this.isRunning) {
 			return;
 		}
 
-		this.interval = window.setInterval(this.taskCallback, this.frequency);
+		this.interval = window.setInterval(this.ticker.run, this.frequency);
 	}
 
 	public stop() {
@@ -22,5 +37,17 @@ export class SuspendableTimer {
 
 	public get isRunning() {
 		return Boolean(this.interval);
+	}
+
+	/**
+	 * Allows executing a function call of the timer
+	 * (tick) without waiting for the timer.
+	 *
+	 * Most useful when the timer is suspended and the
+	 * function is run "manually".
+	 */
+
+	public runTick() {
+		this.ticker.run();
 	}
 }
