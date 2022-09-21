d0c091ee8fcefbcfff5b457dc4ca068795c5d5dc
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-09-21 13:19:28 +0200

    Moved Session processing logic to another method "addChunk" to allow later additions

--- packages/server/src/Session.ts
+++ packages/server/src/Session.ts
@@ -1,7 +1,7 @@
 import type { RawTrack } from "./model";
 import { CueNode } from "./CueNode.js";
 import { IntervalBinaryTree } from "./IntervalBinaryTree.js";
-import { HSBaseRendererConstructor, ParseResult } from "./BaseRenderer/index.js";
+import { HSBaseRenderer, HSBaseRendererConstructor, ParseResult } from "./BaseRenderer/index.js";
 import {
 	UncaughtParsingExceptionError,
 	UnexpectedDataFormatError,
@@ -14,58 +14,18 @@ const activeTrackSymbol = Symbol("session.active");
 export class HSSession {
 	private timelines: { [lang: string]: IntervalBinaryTree<CueNode> } = Object.create(null);
 	private [activeTrackSymbol]: string = null;
+	private renderer: InstanceType<HSBaseRendererConstructor>;
+	private onSafeFailure: (error: Error) => void;
 
 	constructor(
 		rawContents: RawTrack[],
-		renderer: InstanceType<HSBaseRendererConstructor>,
-		onSafeFailure: (error: Error) => void,
+		renderer: HSSession["renderer"],
+		onSafeFailure: HSSession["onSafeFailure"],
 	) {
-		const { rendererName } = Object.getPrototypeOf(renderer)
-			.constructor as HSBaseRendererConstructor;
+		this.renderer = renderer;
+		this.onSafeFailure = onSafeFailure;
 
-		try {
-			for (const { lang, content } of rawContents) {
-				const parseResult = renderer.parse(content);
-
-				if (!(parseResult instanceof ParseResult)) {
-					/** If parser fails once for this reason, it is worth to stop the whole ride. */
-					throw new UnexpectedParsingOutputFormatError(rendererName, lang, parseResult);
-				}
-
-				if (parseResult.data.length) {
-					this.timelines[lang] = new IntervalBinaryTree();
-
-					for (const cue of parseResult.data) {
-						if (!(cue instanceof CueNode)) {
-							parseResult.errors.push({
-								error: new UnexpectedDataFormatError(rendererName),
-								failedChunk: cue,
-								isCritical: false,
-							});
-
-							continue;
-						}
-
-						this.timelines[lang].addNode(cue);
-					}
-				} else if (parseResult.errors.length >= 1) {
-					throw new UnparsableContentError(rendererName, parseResult.errors[0]);
-				}
-
-				for (const parseResultError of parseResult.errors) {
-					onSafeFailure(parseResultError.error);
-				}
-			}
-		} catch (err: unknown) {
-			if (
-				err instanceof UnexpectedParsingOutputFormatError ||
-				err instanceof UnparsableContentError
-			) {
-				throw err;
-			}
-
-			throw new UncaughtParsingExceptionError(rendererName, err);
-		}
+		this.addChunks(...rawContents);
 
 		if (Object.keys(this.timelines).length) {
 			this.activeTrack = Object.keys(this.timelines)[0];
@@ -100,4 +60,68 @@ export class HSSession {
 
 		return this.timelines[this.activeTrack].getCurrentNodes(time);
 	}
+
+	/**
+	 * Allows adding a chunks to be processed by a renderer
+	 * and get inserted into a timeline.
+	 *
+	 * @param tracks
+	 */
+
+	public addChunks(...tracks: RawTrack[]) {
+		try {
+			for (const { lang, content } of tracks) {
+				const parseResult = this.renderer.parse(content);
+
+				if (!(parseResult instanceof ParseResult)) {
+					/** If parser fails once for this reason, it is worth to stop the whole ride. */
+					const rendererName = getRendererName(this.renderer);
+					throw new UnexpectedParsingOutputFormatError(rendererName, lang, parseResult);
+				}
+
+				if (parseResult.data.length) {
+					if (!this.timelines[lang]) {
+						this.timelines[lang] = new IntervalBinaryTree();
+					}
+
+					for (const cue of parseResult.data) {
+						if (!(cue instanceof CueNode)) {
+							const rendererName = getRendererName(this.renderer);
+
+							parseResult.errors.push({
+								error: new UnexpectedDataFormatError(rendererName),
+								failedChunk: cue,
+								isCritical: false,
+							});
+
+							continue;
+						}
+
+						this.timelines[lang].addNode(cue);
+					}
+				} else if (parseResult.errors.length >= 1) {
+					const rendererName = getRendererName(this.renderer);
+					throw new UnparsableContentError(rendererName, parseResult.errors[0]);
+				}
+
+				for (const parseResultError of parseResult.errors) {
+					this.onSafeFailure(parseResultError.error);
+				}
+			}
+		} catch (err: unknown) {
+			if (
+				err instanceof UnexpectedParsingOutputFormatError ||
+				err instanceof UnparsableContentError
+			) {
+				throw err;
+			}
+
+			const rendererName = getRendererName(this.renderer);
+			throw new UncaughtParsingExceptionError(rendererName, err);
+		}
+	}
+}
+
+function getRendererName(renderer: HSBaseRenderer) {
+	return (Object.getPrototypeOf(renderer).constructor as HSBaseRendererConstructor).rendererName;
 }
