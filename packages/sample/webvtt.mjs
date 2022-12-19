414846b98ec819eb06ce39becfea551d8a8fe9f4
Alexander Cerutti <cerutti.alexander@gmail.com>
2022-12-19 20:30:44 +0100

    Moved webvtt script to its own mjs file and fixed commit issue

---
+++ packages/sample/webvtt.mjs
@@ -0,0 +1,61 @@
+import "./src/customElements/scheduled-textarea";
+
+/**
+ * @type {string} text
+ * @returns {string}
+ */
+
+function createTrackURL(text) {
+	const blob = new Blob([text], { type: "text/vtt" });
+	return URL.createObjectURL(blob);
+}
+
+/**
+ * @type {string} trackUrl
+ */
+
+function disposeTrackURL(trackUrl) {
+	URL.revokeObjectURL(trackUrl);
+}
+
+const videoContainer = document.getElementById("video-container");
+const scheduledTextArea = document.getElementsByTagName("scheduled-textarea")?.[0];
+
+scheduledTextArea.addEventListener("commit", ({ detail: text }) => {
+	const currentVideo = videoContainer.querySelector("video");
+	const currentTrack = Array.prototype.find.call(
+		currentVideo.childNodes,
+		(child) => child.nodeName === "TRACK",
+	);
+
+	if (currentTrack?.src) {
+		disposeTrackURL(currentTrack.src);
+	}
+
+	const newTrackURL = createTrackURL(text);
+
+	/**
+	 * Creating again the video tag due to a bug in Chrome
+	 * for which removing a textTrack element and adding a new one
+	 * lefts the UI dirty
+	 */
+
+	const videoElement = Object.assign(document.createElement("video"), {
+		controls: true,
+		muted: true,
+		src: "./big_buck_bunny_1080p_60fps.mp4",
+		autoplay: true,
+	});
+
+	const track = Object.assign(document.createElement("track"), {
+		src: newTrackURL,
+		mode: "showing",
+		default: true,
+		label: "Test track",
+	});
+
+	videoElement.appendChild(track);
+
+	videoContainer.querySelector("video").remove();
+	videoContainer.appendChild(videoElement);
+});
