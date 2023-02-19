d06f5fdd133425a9a37ea7417f315cec89e7cb0c
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-02-19 17:11:36 +0100

    Added build options for vite

--- packages/sample/vite.config.mjs
+++ packages/sample/vite.config.mjs
@@ -3,6 +3,7 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import checker from "vite-plugin-checker";
+import path from "node:path";
 
 /**
  * This file was a .ts file but vite-plugin-checker somehow
@@ -26,6 +27,15 @@ export default defineConfig({
 		host: "0.0.0.0",
 		port: 3000,
 		strictPort: true,
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				index: path.resolve(__dirname, "index.html"),
+				"native-video": path.resolve(__dirname, "pages/native-video/index.html"),
+				"sub37-example": path.resolve(__dirname, "pages/sub37-example/index.html"),
+			},
+		},
 	},
 	assetsInclude: ["**/*.vtt"],
 });
