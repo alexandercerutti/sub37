f84212c0c2093d5142d5a33f8f44910de6c57c55
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-05-01 19:14:17 +0200

    Removed toString from TTML Adapter

--- packages/ttml-adapter/src/Adapter.ts
+++ packages/ttml-adapter/src/Adapter.ts
@@ -1,16 +1,8 @@
 import { BaseAdapter } from "@sub37/server";
 
-export default class Adapter extends BaseAdapter {
+export default class TTMLAdapter extends BaseAdapter {
 	static override get supportedType() {
 		return "application/ttml+xml";
-	}
-
-	static override toString(): string {
-		return "TTML Adapter";
-	}
-
-	override toString(): string {
-		return "TTML Adapter";
 	}
 
 	override parse(rawContent: string): BaseAdapter.ParseResult {
