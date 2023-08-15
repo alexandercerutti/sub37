6c3621993fd24c4662f7fdb035018b28eadd829f
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-08-15 02:22:13 +0200

    Added correct time parsing to parseTimeString

--- packages/ttml-adapter/src/Parser/parseCue.ts
+++ packages/ttml-adapter/src/Parser/parseCue.ts
@@ -1,4 +1,7 @@
-import { TIME_METRIC_UNIT_REGEX } from "./TimeExpressions/TimeUnits";
+import { getTimeBaseProvider } from "./TimeBase/index.js";
+import { matchClockTimeExpression } from "./TimeExpressions/clockTime.js";
+import { matchOffsetTimeExpression } from "./TimeExpressions/offsetTime.js";
+import { matchWallClockTimeExpression } from "./TimeExpressions/wallclockTime.js";
 
 interface TimeDetails {
 	"ttp:timeBase": "media" | "smpte" | "clock";
@@ -10,15 +13,37 @@ interface TimeDetails {
 }
 
 function parseTimeString(timeString: string, timeDetails: TimeDetails): number {
+	const timeProvider = getTimeBaseProvider(timeDetails["ttp:timeBase"]);
+
 	{
-		const match = timeString.match(CLOCK_TIME_REGEX);
+		const match = matchClockTimeExpression(timeString);
+
+		if (match) {
+			return timeProvider.getMillisecondsByClockTime(match, timeDetails);
+		}
 	}
 
 	{
-		const match = timeString.match(OFFSET_TIME_REGEX);
+		const match = matchOffsetTimeExpression(timeString);
+
+		if (match) {
+			return timeProvider.getMillisecondsByOffsetTime(match, timeDetails);
+		}
 	}
 
 	{
-		const match = timeString.match(WALLCLOCK_TIME_REGEX);
+		const match = matchWallClockTimeExpression(timeString);
+
+		if (match) {
+			return timeProvider.getMillisecondsByWallClockTime(match);
+		}
 	}
+
+	/**
+	 * @TODO improve error type here
+	 */
+
+	throw new Error(
+		"Time format didn't match any supported format (ClockTime, OffsetTime or WallClock);",
+	);
 }
