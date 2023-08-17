ece1d62047445b8a872a3192090e0abfb938d62c
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-08-17 02:25:19 +0200

    Added LogicalGroupingContext with tests for styles

---
+++ packages/ttml-adapter/src/Parser/LogicalGroupingContext.ts
@@ -0,0 +1,100 @@
+import type { TTMLStyle } from "./parseStyle";
+
+/**
+ * A LogicalGroupingContext is meant to represent
+ * a 'div' element or a global context that will
+ * contain contextual information
+ */
+
+interface StylesIterableIterator extends IterableIterator<TTMLStyle> {
+	length: number;
+}
+
+const parentSymbol = Symbol("parent");
+const stylesSymbol = Symbol("styles");
+const beginTimeSymbol = Symbol("begin");
+const endTimeSymbol = Symbol("end");
+const durTimeSymbol = Symbol("dur");
+
+export class LogicalGroupingContext {
+	private [parentSymbol]: LogicalGroupingContext | undefined;
+	private [stylesSymbol]: TTMLStyle[] = [];
+
+	private [beginTimeSymbol]: string;
+	private [endTimeSymbol]: string;
+	private [durTimeSymbol]: string;
+
+	constructor(parent?: LogicalGroupingContext) {
+		this[parentSymbol] = parent;
+	}
+
+	public get parent(): LogicalGroupingContext {
+		return this[parentSymbol];
+	}
+
+	public addStyles(...style: TTMLStyle[]): void {
+		this[stylesSymbol].push(...style);
+	}
+
+	public set begin(timeExpression: string) {
+		this[beginTimeSymbol] = timeExpression;
+	}
+
+	public get begin(): string {
+		return this[beginTimeSymbol] || this.parent?.begin;
+	}
+
+	public set end(timeExpression: string) {
+		this[endTimeSymbol] = timeExpression;
+	}
+
+	public get end(): string {
+		return this[endTimeSymbol] || this.parent?.end;
+	}
+
+	public set duration(timeExpression: string) {
+		this[durTimeSymbol] = timeExpression;
+	}
+
+	public get duration(): string {
+		return this[durTimeSymbol] || this.parent?.duration;
+	}
+
+	public get styles(): StylesIterableIterator {
+		let groupStylesIndex = 0;
+		const groupStyles = this[stylesSymbol];
+		const parentIterator: StylesIterableIterator | undefined = this.parent?.styles;
+
+		return {
+			[Symbol.iterator]() {
+				return this;
+			},
+			next() {
+				/**
+				 * Iterating over current group styles and (somehow recursively?)
+				 * on parent styles, which will iterate over parent styles
+				 */
+
+				if (groupStylesIndex < groupStyles.length) {
+					const value = groupStyles[groupStylesIndex];
+					groupStylesIndex += 1;
+
+					return {
+						value,
+						done: false,
+					};
+				}
+
+				if (parentIterator) {
+					return parentIterator.next();
+				}
+
+				return {
+					value: undefined,
+					done: true,
+				};
+			},
+			length: groupStyles.length + (parentIterator?.length ?? 0),
+		};
+	}
+}
