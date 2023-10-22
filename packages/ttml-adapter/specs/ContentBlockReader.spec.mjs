f865f57fc21ac064d67fb5df98bcdf33fac9fbdf
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-10-23 00:07:13 +0200

    Added ContentBlockReader generator function.
    This emits blocks to be used instead of delegating everything to Adapter

---
+++ packages/ttml-adapter/specs/ContentBlockReader.spec.mjs
@@ -0,0 +1,385 @@
+import { describe, expect, it } from "@jest/globals";
+import { BlockType, getNextContentBlock } from "../lib/Parser/ContentBlockReader.js";
+import { Tokenizer } from "../lib/Parser/Tokenizer.js";
+
+describe("ContentBlockReader", () => {
+	it("should return a paragraph or a region", () => {
+		const testDocument1 = `
+			<tt xml:lang="en">
+				<head>
+					<layout>
+						<region xml:id="r1" tts:color="white" tts:origin="10c 4c" tts:extent="40c 5c"/>
+					</layout>
+				</head>
+				<body>
+					<div begin="0s" end="25s">
+						<p>
+							<span begin="0s">Lorem</span>
+							<span begin="1s">ipsum</span>
+							<span begin="2s">dolor</span>
+							<span begin="3s">sit</span>
+						</p>
+						<p>
+							<span begin="4s">Amet</span>
+							<span begin="5s">consectetur</span>
+							<span begin="6s">adipiscing</span>
+							<span begin="7s">elit</span>
+						</p>
+						<p>
+							<span begin="8s">Sed</span>
+							<span begin="9s">do</span>
+							<span begin="10s">eiusmod</span>
+							<span begin="11s">tempor</span>
+							<span begin="12s">incididunt </span>
+							<span begin="13s">labore</span>
+						</p>
+						<p>
+							<span begin="14s">et</span>
+							<span begin="15s">dolore</span>
+							<span begin="16s">magna</span>
+							<span begin="17s">aliqua</span>
+						</p>
+						<p>
+							<span begin="18s">Ut</span>
+							<span begin="19s">enim</span>
+							<span begin="20s">ad</span>
+							<span begin="21s">minim</span>
+							<span begin="22s">veniam</span>
+							<span begin="23s">quis,</span>
+							<span begin="24s">nostrud</span>
+						</p>
+					</div>
+				</body>
+			</tt>
+		`;
+
+		const expectedParagraphObjectContent = {
+			content: {
+				content: "p",
+			},
+		};
+
+		const expectedSpanObjectContent = {
+			content: {
+				content: "span",
+			},
+		};
+
+		const expectedRootChildrenNode = {
+			...expectedSpanObjectContent,
+			children: [],
+			parent: expectedParagraphObjectContent,
+		};
+
+		const tokenizerInstance = new Tokenizer(testDocument1);
+		const generator = getNextContentBlock(tokenizerInstance);
+
+		// Ignoring <tt>
+		generator.next();
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.REGION,
+			{
+				content: {
+					content: "layout",
+				},
+				children: [
+					{
+						content: {
+							content: "region",
+							attributes: {
+								"xml:id": "r1",
+								"tts:color": "white",
+								"tts:origin": "10c 4c",
+								"tts:extent": "40c 5c",
+							},
+						},
+					},
+				],
+			},
+		]);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.GROUP,
+			{
+				content: {
+					content: "div",
+					attributes: {
+						begin: "0s",
+						end: "25s",
+					},
+				},
+				children: [],
+			},
+		]);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.CUE,
+			{
+				...expectedParagraphObjectContent,
+				children: [
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+				],
+			},
+		]);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.CUE,
+			{
+				...expectedParagraphObjectContent,
+				children: [
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+				],
+			},
+		]);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.CUE,
+			{
+				...expectedParagraphObjectContent,
+				children: [
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+				],
+			},
+		]);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.CUE,
+			{
+				parent: null,
+				...expectedParagraphObjectContent,
+				children: [
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+				],
+			},
+		]);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.CUE,
+			{
+				parent: null,
+				...expectedParagraphObjectContent,
+				children: [
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+					expectedRootChildrenNode,
+				],
+			},
+		]);
+
+		const lastRound = generator.next();
+
+		expect(lastRound).toMatchObject({ done: true, value: null });
+	});
+
+	it("should ignore nodes in the wrong place, with a wrong parent relationship", () => {
+		const testDocument1 = `
+			<tt xml:lang="en">
+				<head>
+					<style />
+					<styling>
+						<style xml:id="test" />
+					</styling>
+				</head>
+				<body>
+					<style></style>
+				</body>
+			</tt>
+		`;
+
+		const tokenizerInstance = new Tokenizer(testDocument1);
+		const generator = getNextContentBlock(tokenizerInstance);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.DOCUMENT,
+			{
+				content: {
+					content: "tt",
+					attributes: {
+						"xml:lang": "en",
+					},
+				},
+				children: [],
+			},
+		]);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.STYLE,
+			{
+				parent: null,
+				content: {
+					content: "styling",
+				},
+				children: [
+					{
+						content: {
+							content: "style",
+						},
+						children: [],
+						parent: {
+							content: {
+								content: "styling",
+							},
+						},
+					},
+				],
+			},
+		]);
+	});
+
+	it("should return a region and a styling, if available", () => {
+		const testDocument = `
+			<tt xml:lang="en">
+				<head>
+					<layout>
+						<region xml:id="region_00" />
+						<region xml:id="region_01"/>
+						<region xml:id="region_02">
+							<style xml:id="nested_style_1" />
+							<style xml:id="nested_style_2" />
+							<style xml:id="nested_style_3" />
+						</region>
+					</layout>
+					<styling>
+						<style xml:id="inheritable_style_1" />
+						<style xml:id="inheritable_style_2" />
+						<style xml:id="inheritable_style_3" />
+					</styling>
+				</head>
+				<body></body>
+			</tt>
+		`;
+
+		const tokenizerInstance = new Tokenizer(testDocument);
+		const generator = getNextContentBlock(tokenizerInstance);
+
+		// Ignoring <tt>
+		void generator.next();
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.REGION,
+			{
+				parent: null,
+				content: {
+					content: "layout",
+					attributes: {},
+				},
+				children: [
+					{
+						content: {
+							content: "region",
+							attributes: {
+								"xml:id": "region_00",
+							},
+						},
+						children: [],
+					},
+					{
+						content: {
+							content: "region",
+							attributes: {
+								"xml:id": "region_01",
+							},
+						},
+						children: [],
+					},
+					{
+						content: {
+							content: "region",
+							attributes: {
+								"xml:id": "region_02",
+							},
+						},
+						children: [
+							{
+								content: {
+									content: "style",
+									attributes: {
+										"xml:id": "nested_style_1",
+									},
+								},
+								children: [],
+							},
+							{
+								content: {
+									content: "style",
+									attributes: {
+										"xml:id": "nested_style_2",
+									},
+								},
+								children: [],
+							},
+							{
+								content: {
+									content: "style",
+									attributes: {
+										"xml:id": "nested_style_3",
+									},
+								},
+								children: [],
+							},
+						],
+					},
+				],
+			},
+		]);
+
+		expect(generator.next().value).toMatchObject([
+			BlockType.STYLE,
+			{
+				parent: null,
+				content: {
+					content: "styling",
+					attributes: {},
+				},
+				children: [
+					{
+						content: {
+							content: "style",
+							attributes: {
+								"xml:id": "inheritable_style_1",
+							},
+						},
+						children: [],
+					},
+					{
+						content: {
+							content: "style",
+							attributes: {
+								"xml:id": "inheritable_style_2",
+							},
+						},
+						children: [],
+					},
+					{
+						content: {
+							content: "style",
+							attributes: {
+								"xml:id": "inheritable_style_3",
+							},
+						},
+						children: [],
+					},
+				],
+			},
+		]);
+	});
+});
---
+++ packages/ttml-adapter/src/Parser/ContentBlockReader.ts
@@ -0,0 +1,254 @@
+import { Token } from "./Token";
+import { TokenType } from "./Token.js";
+import type { Node } from "./Tags/Node";
+import { NodeQueue } from "./Tags/NodeQueue.js";
+import type { TreeNode } from "./Tags/NodeTree";
+import { NodeTree } from "./Tags/NodeTree.js";
+import { Tokenizer } from "./Tokenizer.js";
+
+export enum BlockType {
+	IGNORED /****/ = 0b00000001,
+	DOCUMENT /***/ = 0b00000010,
+	HEADER /*****/ = 0b00000100,
+	BODY /*******/ = 0b00001000,
+	REGION /*****/ = 0b00010000,
+	STYLE /******/ = 0b00100000,
+	CUE /********/ = 0b01000000,
+	GROUP /******/ = 0b10000000,
+}
+
+type DocumentBlockTuple = [blockType: BlockType.DOCUMENT, payload: TreeNode<Token>];
+
+type CueBlockTuple = [blockType: BlockType.CUE, payload: TreeNode<Token>];
+
+type HeaderBlockTuple = [blockType: BlockType.HEADER, payload: TreeNode<Token>];
+
+type RegionBlockTuple = [blockType: BlockType.REGION, payload: TreeNode<Token>];
+
+type StyleBlockTuple = [blockType: BlockType.STYLE, payload: TreeNode<Token>];
+
+type GroupBlockTuple = [blockType: BlockType.GROUP, payload: TreeNode<Token>];
+
+type IgnoredBlockTuple = [blockType: BlockType.IGNORED, payload: undefined];
+
+type BlockTuple =
+	| DocumentBlockTuple
+	| CueBlockTuple
+	| HeaderBlockTuple
+	| RegionBlockTuple
+	| StyleBlockTuple
+	| GroupBlockTuple
+	| IgnoredBlockTuple;
+
+const BlockTupleMap = new Map<string, BlockTuple[0]>([
+	["tt", BlockType.DOCUMENT],
+	["head", BlockType.HEADER],
+	["body", BlockType.GROUP],
+	["div", BlockType.GROUP],
+	["layout", BlockType.REGION],
+	["styling", BlockType.STYLE],
+	["p", BlockType.CUE],
+	["span", BlockType.CUE],
+]);
+
+export function* getNextContentBlock(tokenizer: Tokenizer): Iterator<BlockTuple, null, BlockTuple> {
+	const openTagsList = new NodeQueue<Token>();
+	let openTagsTree: NodeTree<Token> | null = null;
+
+	let currentBlockType: BlockType = BlockType.DOCUMENT;
+	let token: Token;
+
+	while ((token = tokenizer.nextToken())) {
+		switch (token.type) {
+			case TokenType.TAG: {
+				if (currentBlockType & BlockType.IGNORED) {
+					continue;
+				}
+
+				if (!isTokenParentRelationshipRespected(token, openTagsList)) {
+					break;
+				}
+
+				if (!openTagsTree) {
+					openTagsTree = new NodeTree(openTagsList);
+				}
+
+				if (openTagsTree) {
+					openTagsTree.track({
+						content: token,
+					});
+				}
+
+				break;
+			}
+
+			case TokenType.START_TAG: {
+				if (currentBlockType & BlockType.IGNORED) {
+					continue;
+				}
+
+				const nodeRepresentation: Node<Token> = {
+					content: token,
+				};
+
+				if (shouldTokenCreateNodeTree(token) && !openTagsTree) {
+					openTagsTree = new NodeTree(openTagsList);
+				}
+
+				if (!isTokenParentRelationshipRespected(token, openTagsList)) {
+					/**
+					 * Even if token does not respect it parent relatioship,
+					 * we still add it to the queue to mark its end later.
+					 *
+					 * We don't want to track it inside the tree, instead,
+					 * because we are going to ignore it.
+					 */
+
+					currentBlockType ^= BlockType.IGNORED;
+					openTagsList.push(nodeRepresentation);
+					continue;
+				}
+
+				switch (token.content) {
+					case "tt": {
+						yield [
+							BlockType.DOCUMENT,
+							NodeTree.createNodeWithParentRelationship(nodeRepresentation, null),
+						];
+
+						continue;
+					}
+
+					case "div": {
+						if (Object.entries(token.attributes).length) {
+							yield [
+								BlockType.GROUP,
+								NodeTree.createNodeWithParentRelationship(nodeRepresentation, null),
+							];
+						}
+
+						continue;
+					}
+
+					case "body": {
+						currentBlockType = BlockType.BODY;
+
+						if (Object.entries(token.attributes).length) {
+							yield [
+								BlockType.GROUP,
+								NodeTree.createNodeWithParentRelationship(nodeRepresentation, null),
+							];
+						}
+
+						continue;
+					}
+
+					case "head": {
+						currentBlockType = BlockType.HEADER;
+						break;
+					}
+				}
+
+				if (openTagsTree) {
+					openTagsTree.push(nodeRepresentation);
+				} else {
+					openTagsList.push(nodeRepresentation);
+				}
+
+				break;
+			}
+
+			case TokenType.END_TAG: {
+				if (!openTagsList.length) {
+					continue;
+				}
+
+				if (openTagsList.current.content.content !== token.content) {
+					continue;
+				}
+
+				if (currentBlockType & BlockType.IGNORED) {
+					currentBlockType ^= BlockType.IGNORED;
+					openTagsList.pop();
+
+					break;
+				}
+
+				if (shouldTokenCreateNodeTree(token) && !openTagsTree?.parentNode) {
+					const blockType: BlockTuple[0] = BlockTupleMap.get(
+						openTagsTree.currentNode.content.content,
+					);
+
+					if (blockType !== BlockType.IGNORED) {
+						yield [blockType, openTagsTree.pop()];
+					}
+
+					openTagsTree = null;
+					break;
+				}
+
+				if (openTagsTree) {
+					openTagsTree.pop();
+				} else {
+					openTagsList.pop();
+				}
+
+				break;
+			}
+		}
+	}
+
+	return null;
+}
+
+function isTokenTreeConstrained(content: string): content is keyof typeof TokenRelationships {
+	return Object.prototype.hasOwnProperty.call(TokenRelationships, content);
+}
+
+const TokenRelationships = {
+	region: ["layout"],
+	style: ["styling", "region"],
+	layout: ["head"],
+	styling: ["head"],
+	head: ["tt"],
+	body: ["tt"],
+	div: ["body"],
+	span: ["span", "p"],
+	p: ["div", "body"],
+} as const;
+
+function isTokenParentRelationshipRespected(
+	token: Token,
+	openTagsQueue: NodeQueue<Token>,
+): boolean {
+	const { content } = token;
+
+	if (!isTokenTreeConstrained(content)) {
+		return true;
+	}
+
+	if (!openTagsQueue.current) {
+		return true;
+	}
+
+	const treeNode: Node<Token> = openTagsQueue.current;
+
+	/**
+	 * If we already reached the point of checking if a tag
+	 * is in a parent, then we don't need to check the others
+	 */
+	for (const direction of TokenRelationships[content]) {
+		if (treeNode.content.content === direction) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+const NODE_TREE_ALLOWED_ELEMENTS = ["p", "span", "layout", "styling"] as const;
+type NODE_TREE_ALLOWED_ELEMENTS = typeof NODE_TREE_ALLOWED_ELEMENTS;
+
+function shouldTokenCreateNodeTree(token: Token): boolean {
+	return NODE_TREE_ALLOWED_ELEMENTS.includes(token.content as NODE_TREE_ALLOWED_ELEMENTS[number]);
+}
