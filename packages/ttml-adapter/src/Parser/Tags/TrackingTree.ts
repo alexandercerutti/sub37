bacab179c5e841ae12514f5e7ce98bdf43e7a556
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-10-29 02:51:53 +0200

    Moved tracked property in TrackableNode to use a symbol and added new static method isTracked

--- packages/ttml-adapter/src/Parser/Tags/TrackingTree.ts
+++ packages/ttml-adapter/src/Parser/Tags/TrackingTree.ts
@@ -1,17 +1,25 @@
 import { NodeTree, NodeWithRelationship } from "./NodeTree.js";
 
+const trackingSymbol = Symbol("tracking");
+
 interface TrackedNode {
-	tracked: true;
+	[trackingSymbol]: true;
 }
 
 interface UntrackedNode {
-	tracked: false;
+	[trackingSymbol]: false;
 }
 
 type TrackableNode<T extends object> = T & (TrackedNode | UntrackedNode);
 
 export class TrackingTree<ContentType extends object> {
 	private tree: NodeTree<TrackableNode<ContentType>> = new NodeTree<TrackableNode<ContentType>>();
+
+	public static isTracked<ContentType extends object>(
+		node: ContentType,
+	): node is ContentType & TrackedNode {
+		return Boolean((node as TrackableNode<ContentType>)[trackingSymbol]);
+	}
 
 	public addTrackedNode(node: ContentType): void {
 		const trackedNode = createTrackedNode(node);
@@ -26,7 +34,7 @@ export class TrackingTree<ContentType extends object> {
 	public pop(): NodeWithRelationship<TrackableNode<ContentType>> | null {
 		const { parentNode } = this.tree;
 
-		if (parentNode?.content.tracked) {
+		if (TrackingTree.isTracked(parentNode?.content)) {
 			this.tree.ascend();
 			return null;
 		}
@@ -41,7 +49,7 @@ export class TrackingTree<ContentType extends object> {
 
 function createTrackedNode(content: object): TrackedNode {
 	return Object.create(content, {
-		tracked: {
+		[trackingSymbol]: {
 			value: true,
 		},
 	} satisfies PropertyDescriptorMap);
@@ -49,7 +57,7 @@ function createTrackedNode(content: object): TrackedNode {
 
 function createUntrackedNode(content: object): UntrackedNode {
 	return Object.create(content, {
-		tracked: {
+		[trackingSymbol]: {
 			value: false,
 		},
 	} satisfies PropertyDescriptorMap);
