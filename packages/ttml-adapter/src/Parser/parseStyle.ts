9ea28e6df787c08108e299419356cff871139292
Alexander Cerutti <cerutti.alexander@gmail.com>
2024-04-01 02:02:30 +0200

    Added styles-to-css attributes remapping map

--- packages/ttml-adapter/src/Parser/parseStyle.ts
+++ packages/ttml-adapter/src/Parser/parseStyle.ts
@@ -102,3 +102,241 @@ function resolveIDREFConflict(idrefsMap: Map<string, TTMLStyle>, id: string): st
 		`--${styleConflictOverrideIdentifier + 1}`,
 	);
 }
+
+/**
+ * @see https://www.w3.org/TR/ttml2/#style-attribute-derivation
+ */
+
+type MappedProperty<Destination extends string> = [Destination, unknown];
+type GenericMapper<T extends string = string> = (
+	value: string,
+) => ReadonlyArray<MappedProperty<T> | undefined>;
+
+function nullMapper(value: unknown): undefined {
+	return undefined;
+}
+
+function defaultValueMapper<T>(value: T): T {
+	return value;
+}
+
+function createPassThroughMapper<Dest extends string>(
+	destinationValue: Dest,
+	valueMapper: (value: unknown) => unknown = defaultValueMapper,
+): GenericMapper<Dest> {
+	return function (value: unknown) {
+		return [[destinationValue, valueMapper(value)]];
+	};
+}
+
+const TTML_CSS_ATTRIBUTES_MAP = {
+	"tts:backgroundClip": createPassThroughMapper("background-clip"),
+	"tts:backgroundColor": createPassThroughMapper("background-color"),
+	"tts:backgroundExtent": createPassThroughMapper("background-size"),
+	"tts:backgroundImage": createPassThroughMapper("background-image"),
+	"tts:backgroundOrigin": createPassThroughMapper("background-origin"),
+	"tts:backgroundPosition": createPassThroughMapper("background-position"),
+	"tts:backgroundRepeat": createPassThroughMapper("background-repeat", backgroundRepeatValueMapper),
+	"tts:border": createPassThroughMapper("border"),
+	// not known
+	"tts:bpd": nullMapper,
+	"tts:color": createPassThroughMapper("color"),
+	"tts:direction": createPassThroughMapper("direction"),
+	// ttml-only
+	"tts:disparity": nullMapper,
+	"tts:display": createPassThroughMapper("display"),
+	"tts:displayAlign": createPassThroughMapper("justify-content", displayAlignValueMapper),
+	// Maps to two CSS. We handle this in a different way
+	"tts:extent": nullMapper,
+	"tts:fontFamily": createPassThroughMapper("font-family"),
+	"tts:fontKerning": createPassThroughMapper("font-kerning"),
+	// No CSS equivalent
+	"tts:fontSelectionStrategy": nullMapper,
+	// Maps to CSS values. Must be handled differently
+	"tts:fontShear": nullMapper,
+	"tts:fontSize": createPassThroughMapper("font-size"),
+	"tts:fontStyle": createPassThroughMapper("font-style"),
+	"tts:fontVariant": nullMapper, // Maps to multiple values. Must be handled differently
+	"tts:fontWeight": createPassThroughMapper("font-weight"),
+	"tts:ipd": nullMapper, // ???????
+	"tts:letterSpacing": createPassThroughMapper("letter-spacing"),
+	"tts:lineHeight": createPassThroughMapper("line-height"),
+	// Maps to CSS values. Must be handled differently
+	"tts:lineShear": nullMapper,
+	// ttml only
+	"tts:luminanceGain": nullMapper,
+	"tts:opacity": createPassThroughMapper("opacity"),
+	"tts:origin": nullMapper, // no css
+	"tts:overflow": createPassThroughMapper("overflow"),
+	"tts:padding": createPassThroughMapper("padding", paddingValueMapper),
+	"tts:position": createPassThroughMapper("background-position"),
+	"tts:ruby": nullMapper,
+	"tts:rubyAlign": createPassThroughMapper("ruby-align"),
+	"tts:rubyPosition": createPassThroughMapper("ruby-position"),
+	"tts:rubyReserve": nullMapper,
+	"tts:shear": nullMapper,
+	"tts:showBackground": nullMapper,
+	"tts:textAlign": createPassThroughMapper("text-align"),
+	"tts:textCombine": createPassThroughMapper("text-combine-upright"),
+	"tts:textDecoration": createPassThroughMapper("text-combine-upright", textDecorationValueMapper),
+	"tts:textEmphasis": createPassThroughMapper("tts:textEmphasis"),
+	"tts:textOrientation": createPassThroughMapper("text-orientation", textOrientationValueMapper),
+	"tts:textOutline": createPassThroughMapper("outline"),
+	"tts:textShadow": createPassThroughMapper("text-shadow"),
+	"tts:unicodeBidi": createPassThroughMapper("unicode-bidi"),
+	"tts:visibility": createPassThroughMapper("visibility"),
+	// XLFO, not a direct mapping with CSS. Can use remap it somehow without impacting renderer?
+	"tts:wrapOption": nullMapper,
+	// Writing mode impacts rendering, so we must first verify nothing will break on that
+	"tts:writingMode": nullMapper,
+	// valid CSS, but it won't be used until we won't paint on a new layer or an absolute element...
+	"tts:zIndex": nullMapper,
+} as const satisfies { [key: string]: GenericMapper };
+
+function backgroundRepeatValueMapper(
+	value: "repeatX" | "repeatY" | "noRepeat" | "inherit",
+): "repeat-x" | "repeat-y" | "no-repeat" | "inherit" {
+	switch (value) {
+		case "repeatX": {
+			return "repeat-x";
+		}
+
+		case "repeatY": {
+			return "repeat-y";
+		}
+
+		case "noRepeat": {
+			return "no-repeat";
+		}
+
+		case "inherit": {
+			return "inherit";
+		}
+
+		default: {
+			return value;
+		}
+	}
+}
+
+function displayAlignValueMapper(
+	value: "before" | "center" | "after" | "justify",
+): "flex-start" | "center" | "flex-end" | "space-between" | undefined {
+	switch (value) {
+		case "before": {
+			return "flex-start";
+		}
+
+		case "center": {
+			return "center";
+		}
+
+		case "after": {
+			return "flex-end";
+		}
+
+		case "justify": {
+			return "space-between";
+		}
+
+		default: {
+			return undefined;
+		}
+	}
+}
+
+function paddingValueMapper(value: string): string | undefined {
+	if (!value.length) {
+		return undefined;
+	}
+
+	switch (value) {
+		case "before": {
+			return "block-start";
+		}
+
+		case "end": {
+			return "inline-end";
+		}
+
+		case "after": {
+			return "block-end";
+		}
+
+		case "start": {
+			return "inline-start";
+		}
+	}
+
+	return value;
+}
+
+/**
+ * @TODO TTML allows several values that CSS do not expect,
+ * like `"noUnderline"`, `"noLineThrough"`, `"noOverline"`.
+ *
+ * However, resetting a is a difficult matter, as it requires
+ * creating a span and setting it to have `display: inline-block`.
+ *
+ * This is important as introducing such style, might compromise
+ * the whole rendering in the captions-renderer.
+ *
+ * Further tests should be performed.
+ *
+ * Just for reference for when this will be implemented, to perform
+ * a reset of text-decoration and apply a different effect, two nested
+ * spans should be produced:
+ *
+ * @example
+ * ```html
+ * <div style="text-decoration: underline">
+ * 	<p>
+ * 		Lorem ipsum dolor sit amet, consectetur
+ * 		<span style="text-decoration: none; display: inline-block">
+ * 			<span style="text-decoration: line-through"> adipiscing elit </span>
+ * 		</span>
+ * 	</p>
+ * 	. Curabitur tempor vitae augue lobortis rutrum. Nam nisi enim, lobortis
+ * </div>
+ * ```
+ *
+ * Producing two spans would mean, probably, to emit a "style reset entity"
+ * or something like that.
+ *
+ * As this is right now too complex to achieve, I rathered to disable
+ * the `text-decoration` property at all. Sorry folks.
+ *
+ * @see https://www.w3.org/TR/ttml2/#style-attribute-textDecoration
+ */
+
+function textDecorationValueMapper(_value: string): string | undefined {
+	// See note above
+	return undefined;
+}
+
+function isTextOrientationSupportedCSSValue(
+	value: string,
+): value is "sideways" | "mixed" | "upright" {
+	return ["sideways", "mixed", "upright"].includes(value);
+}
+
+function textOrientationValueMapper(value: string): "sideways" | "mixed" | "upright" | undefined {
+	if (isTextOrientationSupportedCSSValue(value)) {
+		return value;
+	}
+
+	switch (value) {
+		case "sideways-left":
+		// Kept only for compatibility. Remapping.
+		case "sideways-right": {
+			return "sideways";
+		}
+
+		case "use-glyph-orientation": {
+			// Invalid, as it is used only for SVG
+			return undefined;
+		}
+	}
+
+	return undefined;
+}
