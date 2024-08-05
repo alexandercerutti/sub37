3778e4defff53526cb18949afda3c42acc88a219
Alexander Cerutti <cerutti.alexander@gmail.com>
2024-08-06 00:55:40 +0200

    Fixed clock time base tests and added tests for media time base

--- packages/ttml-adapter/specs/TimeBase/clock.spec.mjs
+++ packages/ttml-adapter/specs/TimeBase/clock.spec.mjs
@@ -6,26 +6,124 @@ import {
 } from "../../lib/Parser/TimeBase/Clock.js";
 
 describe("When timeBase is 'clock'", () => {
+	it("should throw when converting in milliseconds if frames or subframes are passed, when converting to clock-time", () => {
+		expect(() =>
+			getMillisecondsByClockTime([
+				{
+					value: 1,
+					metric: "hours",
+				},
+				{
+					value: 15,
+					metric: "minutes",
+				},
+				{
+					value: 30.5,
+					metric: "seconds",
+				},
+				{
+					value: 25,
+					metric: "frames",
+				},
+				{
+					value: 20,
+					metric: "subframes",
+				},
+			]),
+		).toThrow();
+	});
+
+	it("should not throw when converting in milliseconds if frames or subframes are undefined, when converting to clock-time", () => {
+		expect(() =>
+			getMillisecondsByClockTime([
+				{
+					value: 1,
+					metric: "hours",
+				},
+				{
+					value: 15,
+					metric: "minutes",
+				},
+				{
+					value: 30.5,
+					metric: "seconds",
+				},
+				{
+					value: undefined,
+					metric: "frames",
+				},
+				{
+					value: undefined,
+					metric: "subframes",
+				},
+			]),
+		).not.toThrow();
+
+		expect(() =>
+			getMillisecondsByClockTime([
+				{
+					value: 1,
+					metric: "hours",
+				},
+				{
+					value: 15,
+					metric: "minutes",
+				},
+				{
+					value: 30.5,
+					metric: "seconds",
+				},
+				undefined,
+				undefined,
+			]),
+		).not.toThrow();
+	});
+
 	it("should include only hours, minutes, seconds and fractions when converting to clock-time", () => {
-		expect(getMillisecondsByClockTime([1, 15, 30.5, 25, 20])).toBe(4530500);
+		expect(
+			getMillisecondsByClockTime([
+				{
+					value: 1,
+					metric: "hours",
+				},
+				{
+					value: 15,
+					metric: "minutes",
+				},
+				{
+					value: 30.5,
+					metric: "seconds",
+				},
+				undefined,
+				undefined,
+			]),
+		).toBe(4530500);
 	});
 
 	it("should return the date in milliseconds when converting to wallclock-time", () => {
 		const currentTime = Date.now();
-		expect(getMillisecondsByWallClockTime(new Date())).toBe(currentTime);
+		expect(getMillisecondsByWallClockTime({ value: new Date().getTime(), metric: "date" })).toBe(
+			currentTime,
+		);
 	});
 
 	it("should return the milliseconds offset when converting to offset-time and metric is not a ticks", () => {
-		expect(getMillisecondsByOffsetTime([10.5, "s"], {})).toBe(10500);
-		expect(getMillisecondsByOffsetTime([10, "s"], {})).toBe(10000);
+		expect(getMillisecondsByOffsetTime({ value: 10.5, metric: "s" }, {})).toBe(10500);
+		expect(getMillisecondsByOffsetTime({ value: 10, metric: "s" }, {})).toBe(10000);
 	});
 
 	it("should return the milliseconds offset when converting to offset-time and metric is ticks", () => {
-		expect(getMillisecondsByOffsetTime([10_010_000.1, "t"], { "ttp:tickRate": 10_000_000 })).toBe(
-			1002,
-		);
-		expect(getMillisecondsByOffsetTime([10_010_000, "t"], { "ttp:tickRate": 10_000_000 })).toBe(
-			1001,
-		);
+		expect(
+			getMillisecondsByOffsetTime(
+				{ value: 10_010_000.1, metric: "t" },
+				{ "ttp:tickRate": 10_000_000 },
+			),
+		).toBe(1002);
+		expect(
+			getMillisecondsByOffsetTime(
+				{ value: 10_010_000, metric: "t" },
+				{ "ttp:tickRate": 10_000_000 },
+			),
+		).toBe(1001);
 	});
 });
--- packages/ttml-adapter/specs/TimeBase/media.spec.mjs
+++ packages/ttml-adapter/specs/TimeBase/media.spec.mjs
@@ -1,12 +1,176 @@
 import { describe, expect, it } from "@jest/globals";
+import {
+	getMillisecondsByClockTime,
+	getMillisecondsByOffsetTime,
+	getMillisecondsByWallClockTime,
+} from "../../lib/Parser/TimeBase/Media.js";
 
 describe("When timeBase is 'media'", () => {
-	it.failing("should output a time that includes the previous one, if provided", () => {});
+	it("should return the milliseconds with frames when converting clock-time", () => {
+		expect(
+			getMillisecondsByClockTime(
+				[
+					{
+						value: 1,
+						metric: "hours",
+					},
+					{
+						value: 15,
+						metric: "minutes",
+					},
+					{
+						value: 30.5,
+						metric: "seconds",
+					},
+					{
+						value: 25,
+						metric: "frames",
+					},
+					{
+						value: 0.2,
+						metric: "subframes",
+					},
+				],
+				{
+					"ttp:frameRate": 24,
+					"ttp:subFrameRate": 1, // fallback
+					"ttp:frameRateMultiplier": 1000 / 1001,
+				},
+			),
+		).toBe(4_531_509.3416666667);
+	});
 
-	it.failing(
-		"should include frames, if ttp:frameRate and ttp:frameRateMultiplier are provided",
-		() => {},
-	);
+	it("should use a referenceBegin parameter when converting clock-time", () => {
+		expect(
+			getMillisecondsByClockTime(
+				[
+					{
+						value: 1,
+						metric: "hours",
+					},
+					{
+						value: 15,
+						metric: "minutes",
+					},
+					{
+						value: 30.5,
+						metric: "seconds",
+					},
+					{
+						value: 25,
+						metric: "frames",
+					},
+					{
+						value: 0.2,
+						metric: "subframes",
+					},
+				],
+				{
+					"ttp:frameRate": 24,
+					"ttp:subFrameRate": 1, // fallback
+					"ttp:frameRateMultiplier": 1000 / 1001,
+				},
+				50000,
+			),
+		).toBe(4_581_509.3416666667);
+	});
 
-	it.failing("should include subframes, if provided with them and ttp:subFrameRate", () => {});
+	it("should throw when converting wallclock-time", () => {
+		expect(() =>
+			getMillisecondsByWallClockTime({ value: new Date().getTime(), metric: "date" }),
+		).toThrow();
+	});
+
+	it("should return the milliseconds when converting an offset-time with ticks metric", () => {
+		expect(
+			getMillisecondsByOffsetTime(
+				{ value: 24, metric: "t" },
+				{
+					"ttp:tickRate": 60,
+				},
+			),
+		).toBe(400);
+	});
+
+	it("should return the milliseconds when converting an offset-time with frame metric", () => {
+		expect(
+			getMillisecondsByOffsetTime(
+				{ value: 24.5, metric: "f" },
+				{
+					"ttp:tickRate": 60,
+					"ttp:frameRate": 24,
+					"ttp:subFrameRate": 1, // fallback
+				},
+			),
+		).toBe(1021);
+	});
+
+	it("should return the milliseconds when converting an offset-time with milliseconds metric", () => {
+		expect(
+			getMillisecondsByOffsetTime(
+				{ value: 15.3, metric: "ms" },
+				{
+					"ttp:tickRate": 60,
+					"ttp:frameRate": 24,
+					"ttp:subFrameRate": 1, // fallback
+				},
+			),
+		).toBe(15.3);
+	});
+
+	it("should return the milliseconds when converting an offset-time with seconds metric", () => {
+		expect(
+			getMillisecondsByOffsetTime(
+				{ value: 15.3, metric: "s" },
+				{
+					"ttp:tickRate": 60,
+					"ttp:frameRate": 24,
+					"ttp:subFrameRate": 1, // fallback
+				},
+			),
+		).toBe(15300);
+	});
+
+	it("should return the milliseconds when converting an offset-time with minutes metric", () => {
+		expect(
+			getMillisecondsByOffsetTime(
+				{ value: 15.3, metric: "m" },
+				{
+					"ttp:tickRate": 60,
+					"ttp:frameRate": 24,
+					"ttp:subFrameRate": 1, // fallback
+				},
+			),
+		).toBe(918000);
+	});
+
+	it("should return the milliseconds when converting an offset-time with hours metric", () => {
+		expect(
+			getMillisecondsByOffsetTime(
+				{ value: 15.3, metric: "h" },
+				{
+					"ttp:tickRate": 60,
+					"ttp:frameRate": 24,
+					"ttp:subFrameRate": 1, // fallback
+				},
+			),
+		).toBe(55080000);
+	});
+
+	it("should return the milliseconds when converting an offset-time with unknown metric, by treating it as hours", () => {
+		expect(
+			getMillisecondsByOffsetTime(
+				{
+					value: 15.3,
+					// @ts-expect-error
+					metric: "k",
+				},
+				{
+					"ttp:tickRate": 60,
+					"ttp:frameRate": 24,
+					"ttp:subFrameRate": 1, // fallback
+				},
+			),
+		).toBe(55080000);
+	});
 });
