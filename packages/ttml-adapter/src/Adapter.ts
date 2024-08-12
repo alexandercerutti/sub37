e29c9a13d4f34fce5856e65618489ad828c6ba17
Alexander Cerutti <cerutti.alexander@gmail.com>
2024-08-13 01:16:29 +0200

    Removed ContentBlockReader and merged the logic inside Adapter for better management

--- packages/ttml-adapter/src/Adapter.ts
+++ packages/ttml-adapter/src/Adapter.ts
@@ -1,15 +1,6 @@
 import { BaseAdapter, CueNode } from "@sub37/server";
 import { MissingContentError } from "./MissingContentError.js";
 import { Tokenizer } from "./Parser/Tokenizer.js";
-import {
-	type BlockTuple,
-	getNextContentBlock,
-	isDocumentBlockTuple,
-	isLayoutBlockTuple,
-	isStyleBlockTuple,
-	isContentElementBlockTuple,
-	isCueBlockTuple,
-} from "./Parser/ContentBlockReader.js";
 import { createScope, type Scope } from "./Parser/Scope/Scope.js";
 import { createTimeContext } from "./Parser/Scope/TimeContext.js";
 import { createStyleContext } from "./Parser/Scope/StyleContext.js";
@@ -17,6 +8,88 @@ import type { RegionContextState } from "./Parser/Scope/RegionContext.js";
 import { createRegionContext, findInlineRegionInChildren } from "./Parser/Scope/RegionContext.js";
 import { parseCue } from "./Parser/parseCue.js";
 import { createDocumentContext, readScopeDocumentContext } from "./Parser/Scope/DocumentContext.js";
+import { Token, TokenType } from "./Parser/Token.js";
+import { NodeTree } from "./Parser/Tags/NodeTree.js";
+import { RelationshipTree } from "./Parser/Tags/RelationshipTree.js";
+
+const nodeAttributesSymbol = Symbol("nodeAttributesSymbol");
+
+enum NodeAttributes {
+	NO_ATTRS /********/ = 0b000000,
+	IGNORED /*********/ = 0b000001,
+	GROUP_TRACKED /***/ = 0b000010,
+	PRE_EMITTABLE /***/ = 0b000100,
+	PRE_EMITTED /*****/ = 0b001000,
+}
+
+interface NodeWithAttributes {
+	[nodeAttributesSymbol]: NodeAttributes;
+}
+
+function isNodeIgnored(
+	node: NodeWithAttributes,
+): node is NodeAttributes & { [nodeAttributesSymbol]: NodeAttributes.IGNORED } {
+	return Boolean(node[nodeAttributesSymbol] & NodeAttributes.IGNORED);
+}
+
+function isNodeGroupTracked(
+	node: NodeWithAttributes,
+): node is NodeAttributes & { [nodeAttributesSymbol]: NodeAttributes.GROUP_TRACKED } {
+	return Boolean(node[nodeAttributesSymbol] & NodeAttributes.GROUP_TRACKED);
+}
+
+function isNodePreEmitted(
+	node: NodeWithAttributes,
+): node is NodeAttributes & { [nodeAttributesSymbol]: NodeAttributes.PRE_EMITTED } {
+	return Boolean(node[nodeAttributesSymbol] & NodeAttributes.PRE_EMITTED);
+}
+
+function isNodePreEmittable(
+	node: NodeWithAttributes,
+): node is NodeAttributes & { [nodeAttributesSymbol]: NodeAttributes.PRE_EMITTABLE } {
+	return Boolean(node[nodeAttributesSymbol] & NodeAttributes.PRE_EMITTABLE);
+}
+
+function setNodePreEmitted(node: NodeWithAttributes): void {
+	if (!(node[nodeAttributesSymbol] & NodeAttributes.PRE_EMITTABLE)) {
+		return;
+	}
+
+	node[nodeAttributesSymbol] =
+		(node[nodeAttributesSymbol] & ~NodeAttributes.PRE_EMITTABLE) | NodeAttributes.PRE_EMITTED;
+}
+
+function createNodeWithAttributes<NodeType extends object>(
+	node: NodeType,
+	attributes: NodeAttributes,
+): NodeType & NodeWithAttributes {
+	return Object.create(node, {
+		[nodeAttributesSymbol]: {
+			value: attributes,
+			writable: true,
+		},
+	});
+}
+
+const GROUP_TRACKING_ALLOWED_ELEMENTS = ["p", "span", "layout", "styling"] as const;
+type GROUP_TRACKING_ALLOWED_ELEMENTS = typeof GROUP_TRACKING_ALLOWED_ELEMENTS;
+
+function isTokenAllowedToGroupTrack(token: Token): boolean {
+	return GROUP_TRACKING_ALLOWED_ELEMENTS.includes(
+		token.content as GROUP_TRACKING_ALLOWED_ELEMENTS[number],
+	);
+}
+
+/**
+ * @see https://www.w3.org/TR/2018/REC-ttml2-20181108/#element-vocab-group-table
+ */
+
+const BLOCK_CLASS = ["div", "p"] as const;
+type BLOCK_CLASS = typeof BLOCK_CLASS;
+
+function isBlockClassElement(token: Token): boolean {
+	return BLOCK_CLASS.includes(token.content as BLOCK_CLASS[number]);
+}
 
 export default class TTMLAdapter extends BaseAdapter {
 	public static override get supportedType() {
@@ -37,98 +110,228 @@ export default class TTMLAdapter extends BaseAdapter {
 		let cues: CueNode[] = [];
 		let treeScope: Scope = createScope(undefined, createTimeContext({}), createStyleContext({}));
 
+		const nodeTree = new NodeTree<Token & NodeWithAttributes>();
+		const relationshipTree = new RelationshipTree();
 		const tokenizer = new Tokenizer(rawContent);
-		const blockReader = getNextContentBlock(tokenizer);
 
-		let block: IteratorResult<BlockTuple, BlockTuple>;
+		let token: Token = null;
 
-		while ((block = blockReader.next())) {
-			const { done, value } = block;
+		while ((token = tokenizer.nextToken())) {
+			switch (token.type) {
+				case TokenType.STRING: {
+					if (!nodeTree.currentNode) {
+						continue;
+					}
 
-			if (done) {
-				if (!readScopeDocumentContext(treeScope)) {
-					throw new Error(`Document failed to parse: <tt> element is apparently missing.`);
+					if (isNodeIgnored(nodeTree.currentNode.content)) {
+						continue;
+					}
+
+					nodeTree.track(createNodeWithAttributes(token, NodeAttributes.GROUP_TRACKED));
+					break;
 				}
 
-				break;
-			}
+				case TokenType.START_TAG: {
+					if (nodeTree.currentNode && isNodeIgnored(nodeTree.currentNode.content)) {
+						continue;
+					}
 
-			switch (true) {
-				case isDocumentBlockTuple(value): {
-					if (readScopeDocumentContext(treeScope)) {
+					const relDescriptor = relationshipTree.getDirectionDescriptor(token.content);
+
+					if (!relDescriptor) {
 						/**
-						 * @TODO Change in a fatal error;
+						 * Even if token does not respect it parent relatioship,
+						 * we still add it to the queue to mark its end later.
+						 *
+						 * We don't want to track it inside the tree, instead,
+						 * because we are going to ignore it.
 						 */
-						throw new Error("Malformed TTML track: multiple <tt> were found.");
+
+						nodeTree.push(createNodeWithAttributes(token, NodeAttributes.IGNORED));
+						continue;
 					}
 
-					treeScope.addContext(createDocumentContext(value[1].content.attributes || {}));
-					break;
-				}
+					relDescriptor.navigate();
 
-				case isLayoutBlockTuple(value): {
-					const { children } = value[1];
-
-					const localRegions: RegionContextState[] = [];
-
-					for (const { content: regionToken, children: regionChildren } of children) {
-						if (regionToken.content !== "region") {
-							continue;
+					if (token.content === "tt") {
+						if (readScopeDocumentContext(treeScope)) {
+							/**
+							 * @TODO Change in a fatal error;
+							 */
+							throw new Error("Malformed TTML track: multiple <tt> were found.");
 						}
 
-						localRegions.push({ attributes: regionToken.attributes, children: regionChildren });
+						nodeTree.push(createNodeWithAttributes(token, NodeAttributes.PRE_EMITTED));
+						treeScope.addContext(
+							createDocumentContext(nodeTree.currentNode.content.attributes || {}),
+						);
+						continue;
 					}
 
-					treeScope.addContext(createRegionContext(localRegions));
+					if (token.content === "div" || token.content === "body") {
+						if (isNodePreEmittable(nodeTree.currentNode.content)) {
+							// Pre emitting the previous one before adding another one
+							setNodePreEmitted(nodeTree.currentNode.content);
+							// yield[(BlockType.CONTENT_ELEMENT, nodeTree.currentNode)];
 
-					break;
-				}
+							if (
+								treeScope.parent &&
+								nodeTree.currentNode.parent.content.content !== "div" &&
+								nodeTree.currentNode.content.content === "div"
+							) {
+								treeScope = treeScope.parent;
+							}
 
-				case isStyleBlockTuple(value): {
-					const { children } = value[1];
+							const {
+								children,
+								content: { attributes },
+							} = nodeTree.currentNode;
 
-					const styleTags = children.reduce<Record<string, string>>((acc, { content: token }) => {
-						if (token.content !== "style") {
-							return acc;
+							treeScope = createScope(
+								treeScope,
+								createRegionContext(findInlineRegionInChildren(attributes["xml:id"], children)),
+							);
+
+							break;
 						}
 
-						return Object.assign(acc, token.attributes);
-					}, {});
+						nodeTree.push(createNodeWithAttributes(token, NodeAttributes.PRE_EMITTABLE));
 
-					treeScope.addContext(createStyleContext(styleTags));
+						continue;
+					}
 
+					if (isBlockClassElement(token) && isNodePreEmittable(nodeTree.currentNode.content)) {
+						setNodePreEmitted(nodeTree.currentNode.content);
+
+						/**
+						 * Emitting the current node before everything else. We might want to check if
+						 * this is a "div" or a "body" and act in a different way for other kind of elements
+						 * if we'll introduce some more preEmittable nodes.
+						 */
+						// yield[(BlockType.CONTENT_ELEMENT, nodeTree.currentNode)];
+
+						if (
+							treeScope.parent &&
+							nodeTree.currentNode.parent.content.content !== "div" &&
+							nodeTree.currentNode.content.content === "div"
+						) {
+							treeScope = treeScope.parent;
+						}
+
+						const {
+							children,
+							content: { attributes },
+						} = nodeTree.currentNode;
+
+						treeScope = createScope(
+							treeScope,
+							createRegionContext(findInlineRegionInChildren(attributes["xml:id"], children)),
+						);
+
+						break;
+					}
+
+					let nextAttributes: NodeAttributes;
+
+					if (isTokenAllowedToGroupTrack(token)) {
+						nextAttributes |= NodeAttributes.GROUP_TRACKED;
+					} else {
+						nextAttributes |= NodeAttributes.NO_ATTRS;
+					}
+
+					nodeTree.push(createNodeWithAttributes(token, nextAttributes));
 					break;
 				}
 
-				case isContentElementBlockTuple(value): {
+				case TokenType.END_TAG: {
+					if (!nodeTree.currentNode) {
+						continue;
+					}
+
+					if (nodeTree.currentNode.content.content !== token.content) {
+						continue;
+					}
+
 					if (
-						treeScope.parent &&
-						value[1].parent.content.content !== "div" &&
-						value[1].content.content === "div"
+						isNodeIgnored(nodeTree.currentNode.content) ||
+						isNodePreEmitted(nodeTree.currentNode.content)
 					) {
-						treeScope = treeScope.parent;
+						nodeTree.pop();
+						break;
 					}
 
-					const {
-						children,
-						content: { attributes },
-					} = value[1];
-
-					treeScope = createScope(
-						treeScope,
-						createRegionContext(findInlineRegionInChildren(attributes["xml:id"], children)),
+					const relDescriptor = relationshipTree.getDirectionDescriptor(
+						nodeTree.currentNode.parent.content.content,
 					);
 
-					break;
-				}
+					if (!relDescriptor) {
+						break;
+					}
 
-				case isCueBlockTuple(value): {
-					const node = value[1];
-					cues.push(...parseCue(node, treeScope));
+					relDescriptor.navigate();
 
+					if (
+						isTokenAllowedToGroupTrack(token) &&
+						!isNodeGroupTracked(nodeTree.currentNode.parent.content)
+					) {
+						const currentTag = nodeTree.currentNode.content.content;
+						const currentElement = nodeTree.pop();
+
+						if (currentTag === "layout") {
+							const { children } = currentElement;
+
+							const localRegions: RegionContextState[] = [];
+
+							for (const { content: regionToken, children: regionChildren } of children) {
+								if (regionToken.content !== "region") {
+									continue;
+								}
+
+								localRegions.push({ attributes: regionToken.attributes, children: regionChildren });
+							}
+
+							treeScope.addContext(createRegionContext(localRegions));
+
+							break;
+						}
+
+						if (currentTag === "styling") {
+							const { children } = currentElement;
+
+							const styleTags = children.reduce<Record<string, string>>(
+								(acc, { content: token }) => {
+									if (token.content !== "style") {
+										return acc;
+									}
+
+									return Object.assign(acc, token.attributes);
+								},
+								{},
+							);
+
+							treeScope.addContext(createStyleContext(styleTags));
+
+							break;
+						}
+
+						if (currentTag === "p" || currentTag === "span") {
+							const node = currentElement;
+							cues.push(...parseCue(node, treeScope));
+
+							break;
+						}
+
+						break;
+					}
+
+					nodeTree.pop();
 					break;
 				}
 			}
+		}
+
+		if (!readScopeDocumentContext(treeScope)) {
+			throw new Error(`Document failed to parse: <tt> element is apparently missing.`);
 		}
 
 		return BaseAdapter.ParseResult(cues, []);
