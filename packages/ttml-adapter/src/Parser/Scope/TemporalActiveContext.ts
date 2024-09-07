c16ef19ad0ae66dc57f56f446e7a1d24653ba88f
Alexander Cerutti <cerutti.alexander@gmail.com>
2024-09-08 01:47:21 +0200

    Added style idref acceptance and resolvign in TemporalActiveContext

--- packages/ttml-adapter/src/Parser/Scope/TemporalActiveContext.ts
+++ packages/ttml-adapter/src/Parser/Scope/TemporalActiveContext.ts
@@ -5,17 +5,75 @@
  */
 
 import type { Context, ContextFactory, Scope } from "./Scope.js";
+import { readScopeStyleContainerContext } from "./StyleContainerContext.js";
+import { TTMLStyle } from "../parseStyle.js";
 
 const temporalActiveContextSymbol = Symbol("temporal.active.context");
 
-interface TemporalActiveContext extends Context<TemporalActiveContext> {}
+interface TemporalActiveContext extends Context<TemporalActiveContext> {
+	computedStyles: Record<string, string>;
+}
 
-export function createStyleContext(): ContextFactory<TemporalActiveContext> | null {
+interface TemporalActiveInitParams {
+	stylesIDRefs?: string[];
+}
+
+export function createTemporalActiveContext(
+	initParams: TemporalActiveInitParams,
+): ContextFactory<TemporalActiveContext> {
+	const store = Object.assign(
+		{
+			stylesIDRefs: [],
+		},
+		initParams,
+	);
+
+	let computedStyleCache: TTMLStyle["attributes"] | null = null;
+
 	return function (scope: Scope) {
 		return {
 			parent: undefined,
 			identifier: temporalActiveContextSymbol,
-			mergeWith(context: TemporalActiveContext): void {},
+			mergeWith(context: TemporalActiveContext): void {
+				/**
+				 * @TODO add merging logic
+				 */
+			},
+			get computedStyles(): TTMLStyle["attributes"] {
+				if (computedStyleCache) {
+					return computedStyleCache;
+				}
+
+				const idrefs = store.stylesIDRefs;
+
+				if (!idrefs.length) {
+					return this.parent?.computedStyles;
+				}
+
+				const styleContext = readScopeStyleContainerContext(scope);
+
+				if (!styleContext?.styles) {
+					return {};
+				}
+
+				const finalStylesAttributes: TTMLStyle["attributes"] = {};
+
+				for (const idref of idrefs) {
+					const style = styleContext.styles.get(idref);
+
+					if (!style) {
+						continue;
+					}
+
+					Object.assign(finalStylesAttributes, style.attributes);
+				}
+
+				computedStyleCache = finalStylesAttributes;
+				return finalStylesAttributes;
+			},
+			// get regions(): Map<string, TTMLRegion> {
+			// 	return regionsArchive;
+			// },
 		};
 	};
 }
