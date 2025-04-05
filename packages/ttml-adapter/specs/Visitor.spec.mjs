7e2b09e5b126eee247a7adfd5f6ae237173b759c
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-04-05 15:26:57 +0200

    FIxed Visitor tests and imports

--- packages/ttml-adapter/specs/Visitor.spec.mjs
+++ packages/ttml-adapter/specs/Visitor.spec.mjs
@@ -1,13 +1,17 @@
-import { describe, it, expect, beforeEach } from "@jest/globals";
-import { createVisitor } from "../lib/Parser/Tags/Representation/visitor.js";
+import { describe, it, expect } from "@jest/globals";
+import { createVisitor } from "../lib/Parser/structure/visitor.js";
 import { createNode } from "../lib/Parser/Tags/Representation/NodeRepresentation.js";
-import * as Kleene from "../lib/Parser/Tags/Representation/kleene.js";
+import * as Kleene from "../lib/Parser/structure/kleene.js";
+
+/**
+ * @typedef {import("../lib/Parser/Tags/Representation/NodeRepresentation.js").NodeRepresentation<string>} NodeRepresentation
+ */
 
 describe("Visitor", () => {
 	it("should return the first node that matches", () => {
 		const visitor = createVisitor(
-			createNode(null, [], () => [
-				Kleene.zeroOrOne(createNode("test1")),
+			createNode(null, new Set(), () => [
+				Kleene.zeroOrOne(/** @type {NodeRepresentation} */ (createNode("test1"))),
 				Kleene.oneOrMore(createNode("test2")),
 			]),
 		);
@@ -20,10 +24,24 @@ describe("Visitor", () => {
 
 	it("should navigate to the next node and perform a match check on it's destinations and back", () => {
 		const visitor = createVisitor(
-			createNode(null, [], () => [
-				Kleene.zeroOrOne(createNode("test1")),
-				Kleene.oneOrMore(createNode("test2", [], () => [Kleene.oneOrMore(createNode("test3"))])),
-				Kleene.oneOrMore(createNode("test4", [], () => [Kleene.zeroOrOne(createNode("test4"))])),
+			createNode(null, new Set(), () => [
+				Kleene.zeroOrOne(
+					//
+					/** @type {NodeRepresentation} */ (createNode("test1")),
+				),
+				Kleene.oneOrMore(
+					createNode("test2", new Set(), () => [
+						//
+						Kleene.oneOrMore(createNode("test3")),
+					]),
+				),
+				Kleene.oneOrMore(
+					//
+					createNode("test4", new Set(), () => [
+						//
+						Kleene.zeroOrOne(createNode("test4")),
+					]),
+				),
 			]),
 		);
 
