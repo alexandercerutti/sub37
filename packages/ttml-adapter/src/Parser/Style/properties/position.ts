450ea3defb0eb53e3ea03b09f2751be6230ce864
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-12-26 10:37:56 +0100

    Added transformation for Position property

--- packages/ttml-adapter/src/Parser/Style/properties/position.ts
+++ packages/ttml-adapter/src/Parser/Style/properties/position.ts
@@ -1,3 +1,4 @@
+import type { PropertiesCollection } from "../../parseStyle.js";
 import type { Scope } from "../../Scope/Scope.js";
 import type { InferDerivableValue } from "../structure/operators.js";
 import type { PositionGrammar } from "../syntax/position.js";
@@ -8,11 +9,16 @@ export { PositionGrammar as Grammar } from "../syntax/position.js";
 export function cssTransform(
 	_scope: Scope,
 	value: InferDerivableValue<typeof PositionGrammar>,
-): string | null {
-	normalizePositionValue(value);
+): PropertiesCollection<["position", "left", "top"]> | null {
+	const normalizedValue = normalizePositionValue(value);
 
-	/**
-	 * @TODO Process
-	 */
-	return null;
+	if (!normalizedValue) {
+		return null;
+	}
+
+	return [
+		["position", "absolute"],
+		["left", normalizedValue[1].toString()],
+		["top", normalizedValue[3].toString()],
+	];
 }
