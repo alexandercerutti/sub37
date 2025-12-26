979be5aff22738a2d02b7dab5a3ec6d4795dff92
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-12-26 10:43:50 +0100

    Added transformation for backgroundPosition

--- packages/ttml-adapter/src/Parser/Style/properties/backgroundPosition.ts
+++ packages/ttml-adapter/src/Parser/Style/properties/backgroundPosition.ts
@@ -1,10 +1,24 @@
-import { InferDerivableValue } from "../structure/operators.js";
+import type { PropertiesCollection } from "../../parseStyle.js";
+import type { InferDerivableValue } from "../structure/operators.js";
 import { normalizePositionValue } from "../syntax/position.js";
 import type { PositionGrammar } from "../syntax/position.js";
 
 export { PositionGrammar as Grammar } from "../syntax/position.js";
 
-export function cssTransform(value: InferDerivableValue<typeof PositionGrammar>): string | null {
-	normalizePositionValue(value);
-	return null;
+export function cssTransform(
+	value: InferDerivableValue<typeof PositionGrammar>,
+): PropertiesCollection<["background-position"]> | null {
+	const normalizedValue = normalizePositionValue(value);
+
+	if (!normalizedValue) {
+		return null;
+	}
+
+	return [
+		[
+			"background-position",
+			// left <length> top <length>
+			`${normalizedValue[0]} ${normalizedValue[1].toString()} ${normalizedValue[2]} ${normalizedValue[3].toString()}`,
+		],
+	];
 }
