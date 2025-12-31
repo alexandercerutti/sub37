3486eb7b1cabe0509defa013653644271effd8d4
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-12-31 15:38:55 +0100

    Added more properties to `displayAlign` as per specification

--- packages/ttml-adapter/src/Parser/Style/properties/displayAlign.ts
+++ packages/ttml-adapter/src/Parser/Style/properties/displayAlign.ts
@@ -8,22 +8,42 @@ export { DisplayAlignGrammar as Grammar } from "../syntax/display-align.js";
 export function cssTransform(
 	_scope: Scope,
 	outcome: InferDerivableValue<typeof DisplayAlignGrammar>,
-): PropertiesCollection<["justify-content"]> {
+): PropertiesCollection<["display", "flex-direction", "position", "justify-content"]> {
 	switch (outcome[0].value[0]) {
 		case "before": {
-			return [["justify-content", "flex-start"]];
+			return [
+				["display", "flex"],
+				["flex-direction", "column"],
+				["position", "absolute"],
+				["justify-content", "flex-start"],
+			];
 		}
 
 		case "center": {
-			return [["justify-content", "center"]];
+			return [
+				["display", "flex"],
+				["flex-direction", "column"],
+				["position", "absolute"],
+				["justify-content", "center"],
+			];
 		}
 
 		case "after": {
-			return [["justify-content", "flex-end"]];
+			return [
+				["display", "flex"],
+				["flex-direction", "column"],
+				["position", "absolute"],
+				["justify-content", "flex-end"],
+			];
 		}
 
 		case "justify": {
-			return [["justify-content", "space-between"]];
+			return [
+				["display", "flex"],
+				["flex-direction", "column"],
+				["position", "absolute"],
+				["justify-content", "space-between"],
+			];
 		}
 	}
 }
