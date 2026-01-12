22e27a4527e97012d225739795dbf59be33cb0db
Alexander Cerutti <cerutti.alexander@gmail.com>
2026-01-13 00:29:02 +0100

    Fixed textOutline cssTransform implementation

--- packages/ttml-adapter/src/Parser/Style/properties/textOutline.ts
+++ packages/ttml-adapter/src/Parser/Style/properties/textOutline.ts
@@ -9,22 +9,25 @@ export function cssTransform(
 	_scope: Scope,
 	value: InferDerivableValue<typeof TextOutlineGrammar>,
 ): PropertiesCollection<["text-shadow", "-webkit-text-stroke"]> | null {
-	if (value[0].type === "keyword") {
+	if (value.length === 1) {
 		return [
 			["text-shadow", "none"],
 			["-webkit-text-stroke", "0 currentColor"],
 		];
 	}
 
-	const [
-		//
-		{ value: outlineColor },
-		{ value: outlineThickness },
-		{ value: outlineBlurRadius },
-	] = value;
+	const outlineColor: string | undefined = value[0]?.value.value;
+	const outlineThickness: string | undefined = value[1]?.value.value.toString();
+	const outlineBlurRadius: string | undefined = value[2]?.value.value.toString();
+
+	/**
+	 * For some kind of reason, Web Animation API doesn't support animating `-webkit-text-stroke`
+	 * but @keyframes do. Therefore we need to use style tags for animation generation
+	 * instead of WAAPI in the caption-renderer.
+	 */
 
 	return [
-		["text-shadow", `${outlineColor} 1px 1px ${outlineBlurRadius}`],
-		["-webkit-text-stroke", `${outlineThickness} ${outlineColor}`],
+		["text-shadow", `1px 1px ${outlineBlurRadius || 0}px ${outlineColor || "currentColor"}`],
+		["-webkit-text-stroke", `${outlineThickness || 0}px ${outlineColor || "currentColor"}`],
 	];
 }
