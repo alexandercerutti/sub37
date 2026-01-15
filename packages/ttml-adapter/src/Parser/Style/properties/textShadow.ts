0f5f640910adc0764873e9d4eebab4a067a5effe
Alexander Cerutti <cerutti.alexander@gmail.com>
2026-01-15 02:32:37 +0100

    Added improved implementation of cssTransform for TextShadow

--- packages/ttml-adapter/src/Parser/Style/properties/textShadow.ts
+++ packages/ttml-adapter/src/Parser/Style/properties/textShadow.ts
@@ -1,14 +1,15 @@
 import type { PropertiesCollection } from "../../parseStyle.js";
 import type { Scope } from "../../Scope/Scope.js";
-import type { InferDerivableValue } from "../structure/operators.js";
+import type { DerivedValue, InferDerivableValue } from "../structure/operators.js";
 import type { TextShadowGrammar } from "../syntax/text-shadow.js";
+import type { Length } from "../../Units/length.js";
 
 export { TextShadowGrammar as Grammar } from "../syntax/text-shadow.js";
 
 /**
  * TextShadow allows splitting by commas to separate multiple shadows
  * @param input
- * @returns 
+ * @returns
  */
 export function tokenizer(input: string): string[] {
 	return input.split(/\s*(,)\s*|\s+/).filter(Boolean);
@@ -18,5 +19,49 @@ export function cssTransform(
 	_scope: Scope,
 	value: InferDerivableValue<typeof TextShadowGrammar>,
 ): PropertiesCollection<["text-shadow"]> | null {
-	return [["text-shadow", value.map(({ value }) => value.toString()).join(" ")]];
+	if (value.length === 1) {
+		return [["text-shadow", "none"]];
+	}
+
+	type DerivedShadow = [
+		DerivedValue<string, Length>, // offset-x
+		DerivedValue<string, Length>, // offset-y
+		DerivedValue<string, Length> | undefined, // blur-radius
+		DerivedValue<string, string> | undefined, // color
+	];
+
+	let values = value.slice();
+	const buffer: Partial<DerivedValue>[] = [];
+	const shadows: DerivedShadow[] = [];
+
+	while (values.length) {
+		const token = values.shift();
+
+		if (!token) {
+			shadows.push(buffer as DerivedShadow);
+			buffer.length = 0;
+			continue;
+		}
+
+		if (token.value === ",") {
+			shadows.push(buffer as DerivedShadow);
+			buffer.length = 0;
+			continue;
+		}
+
+		buffer.push(token);
+	}
+
+	shadows.push(buffer as DerivedShadow);
+
+	const textShadows = shadows.map((shadow) => {
+		const offsetX = shadow[0].value.toString();
+		const offsetY = shadow[1].value.toString();
+		const blurRadius = shadow[2]?.value.toString();
+		const color = shadow[3]?.value;
+
+		return [offsetX, offsetY, blurRadius, color].filter(Boolean).join("\x20");
+	});
+
+	return [["text-shadow", textShadows.join(",")]];
 }
