c224911b3c2014b7f0a05ce70eda4d878b7029d0
Alexander Cerutti <cerutti.alexander@gmail.com>
2026-01-19 01:38:59 +0100

    Added standard definition comment for animations that specifies how we are not going to support implied definitions for animations

--- packages/ttml-adapter/src/Parser/Animations/parseAnimation.ts
+++ packages/ttml-adapter/src/Parser/Animations/parseAnimation.ts
@@ -172,6 +172,40 @@ function getAnimationValueListByStyleName(
  * for styles that are not animatable or not compatible with the specified
  * animatable style.
  *
+ * Please note that animation-value-list might be defined as follows as well
+ * for multi-components attributes, like `tts:border`:
+ *
+ * ```
+ * tts:border="2px red; green; blue"
+ * ```
+ *
+ * or
+ *
+ * ```xml
+ * <p tts:border="2x solid">
+ * 	<animate tts:border="red; green; blue"/>
+ * 	<span>
+ * 		A paragraph with a 2px, solid border with a linear animated border color
+ * 		transitioning from red to green to blue
+ * 		over implied keyTimes="0;0.5;1".
+ * 	</span>
+ * </p>
+ * ```
+ *
+ * Both of these cases define that there is a default value for `2px solid` (width and style)
+ * and that they do not need to be re-defined in the animation values.
+ *
+ * The grammar system, as was designed, is not able to handle this case yet because it is strict
+ * according to the syntax definitions. Supporting these cases would require a more complex system
+ * that could bypass derivation rejections when checking for animations.
+ *
+ * So, in order to correctly support animations, full properties will need to be defined in each keyframe.
+ *
+ * Furthermore, this behavior is not explicitly defined in the TTML2 specification but rather in the
+ * the emails I exchanged in the W3C TTML mailing list.
+ *
+ * @see https://lists.w3.org/Archives/Public/public-tt/2025Mar/0001.html
+ *
  * @param animatableStyle
  * @param animationValueListByStyleName
  * @returns
