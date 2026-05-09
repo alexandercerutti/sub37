b523e6264562ec19224b58da01f9a77c3ee8027e
Alexander Cerutti <cerutti.alexander@gmail.com>
2026-05-09 22:19:53 +0200

    Removed unused preventZero function

--- packages/ttml-adapter/src/Parser/Units/number.ts
+++ packages/ttml-adapter/src/Parser/Units/number.ts
@@ -1,11 +1,3 @@
 export function asNumbers(values: string[]): number[] {
 	return values.map((e) => parseFloat(e));
 }
-
-export function preventZero(value: number, fallback: number): number | null {
-	if (value === 0) {
-		return fallback;
-	}
-
-	return value;
-}
