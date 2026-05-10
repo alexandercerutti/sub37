33308dc837f9ed316e764dfaf397ae8856d0fac2
Alexander Cerutti <cerutti.alexander@gmail.com>
2026-05-10 17:52:08 +0200

    Adapted origin to use root tts:extent when pixel

--- packages/ttml-adapter/src/Parser/Style/properties/origin.ts
+++ packages/ttml-adapter/src/Parser/Style/properties/origin.ts
@@ -1,12 +1,17 @@
+import { readScopeDocumentContext } from "../../Scope/DocumentContext.js";
+import { readScopeErrorContext } from "../../Scope/ErrorContext.js";
 import type { Scope } from "../../Scope/Scope.js";
+import { createUnit, Unit } from "../../Unit.js";
 import type { PropertiesCollection } from "../../parseStyle.js";
+import { isPercentage } from "../primitives/length.js";
+import { getPixelScalarPercentageConversion, isPixelScalar } from "../primitives/pixel.js";
 import type { InferDerivableValue } from "../structure/operators.js";
 import type { OriginGrammar } from "../syntax/origin.js";
 
 export { OriginGrammar as Grammar } from "../syntax/origin.js";
 
 export function cssTransform(
-	_scope: Scope,
+	scope: Scope,
 	value: InferDerivableValue<typeof OriginGrammar>,
 ): PropertiesCollection<["x", "y"]> {
 	if (value.length === 1) {
@@ -28,12 +33,62 @@ export function cssTransform(
 		];
 	}
 
-	const [{ value: x }, { value: y }] = value;
+	const xDimension = getOriginLengthDimension(scope, value[0], 0);
+	const yDimension = getOriginLengthDimension(scope, value[1], 1);
 
 	return [
-		["x", x.toString()],
-		["y", y.toString()],
+		["x", xDimension.toString()],
+		["y", yDimension.toString()],
 	];
+}
+
+function getOriginLengthDimension(
+	scope: Scope,
+	data: Extract<InferDerivableValue<typeof OriginGrammar>[number], { type: "length" }>,
+	axis: 0 | 1,
+): Unit<"%"> {
+	if (!data) {
+		return createUnit(0, "%");
+	}
+
+	const originWithUnit = data.value;
+
+	if (isPercentage(originWithUnit)) {
+		return originWithUnit;
+	}
+
+	const errorContext = readScopeErrorContext(scope)!;
+
+	if (isPixelScalar(originWithUnit)) {
+		const documentContext = readScopeDocumentContext(scope)!;
+		const documentExtent = documentContext.attributes["tts:extent"];
+
+		if (!documentExtent) {
+			errorContext.report(
+				new Error(
+					`Origin ${axis === 0 ? "x" : "y"} set to a pixel value, but document extent is not defined. Will be treated as 0px.`,
+				),
+				false,
+			);
+
+			return createUnit(0, "%");
+		}
+
+		return getPixelScalarPercentageConversion(documentExtent[axis].value, originWithUnit)!;
+	}
+
+	/**
+	 * @TODO how to handle other length units here?
+	 */
+
+	errorContext.report(
+		new Error(
+			"Origin set to a Length different from px or %, is not supported yet. Will be treated as 0%.",
+		),
+		false,
+	);
+
+	return createUnit(0, "%");
 }
 
 export function validateAnimation(
