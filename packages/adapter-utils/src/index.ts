ed91635bb72dbad2f5d7fc3378f48df1e18c526a
Alexander Cerutti <cerutti.alexander@gmail.com>
2026-06-18 23:20:37 +0200

    Improved import

--- packages/adapter-utils/src/index.ts
+++ packages/adapter-utils/src/index.ts
@@ -3,5 +3,5 @@ export * from "./BaseAdapter/index.js";
 export * from "./Errors/index.js";
 export * from "./CueNode.js";
 export * from "./Region.js";
-export * as Entities from "./Entities";
+export * as Entities from "./Entities/index.js";
 export * from "./RenderingModifiers.js";
